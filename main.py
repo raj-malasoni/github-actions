99211ddf2d1d26c3400a3d9d15156874412aa8bf
raj-malasoni <166719758+raj-malasoni@users.noreply.github.com>
2024-05-01 16:33:31 -0400

    Update main.py
    stale code for gchat updated

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ try:
 
     # Define messages for Google Chat and comment body in Github
     msg = {
-        "stale_label": 'This PR is stale because it has been open 15 days with no activity. Remove stale label or comment/update PR otherwise this will be closed in next 2 days.',
+        "stale_label": 'This PR is stale because it has been open 15 days with no activity. Remove stale label and update PR otherwise this will be closed in next 2 days.',
         "stale_days": 15,
         "stale_close_days": 2,
         "staled_PR_closing": 'This PR was closed because it has been stalled for 2 days with no activity.',
@@ -61,10 +61,12 @@ try:
         # 1. Add "Stale" label to the PR if no activity for 15 days
         for pull in pulls:
             time_diff = now - pull.updated_at
-            if time_diff > timedelta(days=msg.get("stale_days")):
+            stale_label_present = any(label.name == "Stale" for label in pull.labels)
+            if time_diff > timedelta(days=msg.get("stale_days"))and not stale_label_present:
                 pull.create_issue_comment(msg.get("stale_label"))
                 pull.add_to_labels('Stale')
-                GCHAT_MESSAGE.append(msg.get("stale_label"))
+                GCHAT_MESSAGE.append("The pr number:"+ str(pull.number)+" " +msg.get("stale_label"))
+                GCHAT_MESSAGE.append("URL: " + pull.html_url)
         
         # 2. Close stalled PR if no activity for 2 days
         for pull in pulls:
@@ -72,7 +74,8 @@ try:
                 if time_diff > timedelta(days=msg.get("stale_close_days")):
                     pull.edit(state="closed")
                     pull.create_issue_comment(msg.get("staled_PR_closing"))
-                    GCHAT_MESSAGE.append(msg.get("staled_PR_closing"))
+                    GCHAT_MESSAGE.append("The pr number:"+ str(pull.number)+ " "+msg.get("staled_PR_closing"))
+                    GCHAT_MESSAGE.append("URL: " + pull.html_url)
                     
     if EVENT_CHECK =='pull':
         # 3. Check if the PR targets the master branch directly
@@ -135,8 +138,11 @@ try:
     if EVENT_CHECK and GCHAT_WEBHOOK_URL:
         message = msg.get("default")
         message = msg.get(EVENT, message)
-        for n in GCHAT_MESSAGE:
-            message =message +'\nIssue comment : ' + n
+        if EVENT_CHECK =='stale' :
+            message ='\n'.join(GCHAT_MESSAGE)
+        else :
+            for n in GCHAT_MESSAGE:
+                message =message +'\nIssue comment : ' + n
         response = send_message_to_google_chat(message, GCHAT_WEBHOOK_URL)
 
 except Exception as e:
